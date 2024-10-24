35b20d6c8c642c765eb4e27c39787c47da71739f
lastonton <138580872+lastonton@users.noreply.github.com>
2024-10-24 21:19:45 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,3 +8,8 @@ const bodyParsers = require('body-parser')
 app.use(bodyParsers.json({limit: '10mb'}));
 
 app.use('/api/user', require('./api/user.js'));
+
+app.use('/', express.static(path.resolve(__dirname, '../client/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
